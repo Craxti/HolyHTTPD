9c800efb455fc9d8d643858ae53e7d47d6e90d05
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2023-06-30 11:20:49 +0400

    Update alert_handler.py pep8

--- modules/alert_handler.py
+++ modules/alert_handler.py
@@ -4,15 +4,15 @@ import threading
 from datetime import datetime
 
 # Read config file
-with open('config.json') as f:
+with open("config.json") as f:
     config = json.load(f)
 
 # Settings logs
-logging.basicConfig(filename='logs/alerts.log', level=logging.INFO)
+logging.basicConfig(filename="logs/alerts.log", level=logging.INFO)
 
 # Очередь для обработки алертов
-alert_queue = config['alert_queue']
-alert_threads_num = config['alert_threads_num']
+alert_queue = config["alert_queue"]
+alert_threads_num = config["alert_threads_num"]
 
 
 def alert_handler(alert_file):
@@ -20,22 +20,26 @@ def alert_handler(alert_file):
     with open(alert_file) as file_l:
         alert = json.load(file_l)
 
-    alert_id = alert['id']
-    alert_time = datetime.fromtimestamp(alert['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
-    alert_msg = alert['message']
-    alert_src_ip = alert['src_ip']
-    alert_dst_ip = alert['dst_ip']
-    alert_protocol = alert['protocol']
-    alert_port = alert['port']
+    alert_id = alert["id"]
+    alert_time = datetime.fromtimestamp(alert["timestamp"]).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+    alert_msg = alert["message"]
+    alert_src_ip = alert["src_ip"]
+    alert_dst_ip = alert["dst_ip"]
+    alert_protocol = alert["protocol"]
+    alert_port = alert["port"]
 
     # Format message for log file
-    log_msg = f"ALERT ID: {alert_id}\n" \
-              f"TIME: {alert_time}\n" \
-              f"MESSAGE: {alert_msg}\n" \
-              f"SOURCE IP: {alert_src_ip}\n" \
-              f"DESTINATION IP: {alert_dst_ip}\n" \
-              f"PROTOCOL: {alert_protocol}\n" \
-              f"PORT: {alert_port}\n"
+    log_msg = (
+        f"ALERT ID: {alert_id}\n"
+        f"TIME: {alert_time}\n"
+        f"MESSAGE: {alert_msg}\n"
+        f"SOURCE IP: {alert_src_ip}\n"
+        f"DESTINATION IP: {alert_dst_ip}\n"
+        f"PROTOCOL: {alert_protocol}\n"
+        f"PORT: {alert_port}\n"
+    )
 
     logging.info(log_msg)
 
