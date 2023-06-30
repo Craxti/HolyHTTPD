2b3c2d1374485f3d32d6c30163f51394c7024f21
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2023-06-30 11:21:09 +0400

    Update packet_sniffer.py pep8

--- modules/packet_sniffer.py
+++ modules/packet_sniffer.py
@@ -6,11 +6,11 @@ import json
 
 from scapy.all import AsyncSniffer
 
-logging.basicConfig(filename='logs/packet_sniffer.log', level=logging.INFO)
+logging.basicConfig(filename="logs/packet_sniffer.log", level=logging.INFO)
 
 
 class PacketSniffer:
-    def __init__(self, pcap_queue, output_format='pcap', *args):
+    def __init__(self, pcap_queue, output_format="pcap", *args):
         self.pcap_queue = pcap_queue
         self.output_format = output_format.lower()
         self.sniffing_thread = None
@@ -18,7 +18,8 @@ class PacketSniffer:
     def start_sniffing(self, interface=None, filter=None):
         if self.sniffing_thread is None or not self.sniffing_thread.is_alive():
             self.sniffing_thread = threading.Thread(
-                target=self._sniff, args=(interface, filter))
+                target=self._sniff, args=(interface, filter)
+            )
             self.sniffing_thread.start()
             logging.info("Packet sniffing started")
 
@@ -27,17 +28,23 @@ class PacketSniffer:
         pcap_queue = queue.Queue()
 
         # добавить элементы в очередь
-        pcap_queue.put('pcap/capture.pcap')
+        pcap_queue.put("pcap/capture.pcap")
 
         # передать очередь в config
-        config = {'pcap_queue': pcap_queue}
+        config = {"pcap_queue": pcap_queue}
 
         # получить элементы из очереди
-        pcap_file = config['pcap_queue'].get()
-        logging.info('Received pcap file %s', pcap_file)
+        pcap_file = config["pcap_queue"].get()
+        logging.info("Received pcap file %s", pcap_file)
 
         # Добавить обработку pcap-файла
-        t = threading.Thread(target=pcap_parser, args=(pcap_file, pcap_queue,))
+        t = threading.Thread(
+            target=pcap_parser,
+            args=(
+                pcap_file,
+                pcap_queue,
+            ),
+        )
         t.start()
 
     def stop_sniffing(self):
@@ -47,9 +54,9 @@ class PacketSniffer:
             logging.info("Packet sniffing stopped")
 
     async def _process_packet(self, packet):
-        if self.output_format == 'json':
+        if self.output_format == "json":
             # Convert the packet to JSON format
-            json_packet = {'src': packet.ip.src, 'dst': packet.ip.dst}
+            json_packet = {"src": packet.ip.src, "dst": packet.ip.dst}
             json_packet_str = json.dumps(json_packet)
 
             # Add the JSON packet to the queue
